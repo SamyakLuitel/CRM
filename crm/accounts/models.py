6737fdee948b95f42aa9a96d70999c474e62edbc
Samk <samkotaku@gmail.com>
2021-05-16 14:50:58 +0545

    worked on templates , frontend  and solved static file issues, models  for django updated

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -3,13 +3,19 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length =200, null = True)
-    phone = models.IntegerField(max_length=20, null = True)
+    phone = models.IntegerField( null = True)
     email = models.EmailField(null=  True)
     date_created =  models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
     
+
+class Tag(models.Model):
+    name = models.CharField(max_length = 200, null =True)
+
+    def __str__(self):
+        return self.name 
 
 
 class Product(models.Model):
@@ -20,13 +26,20 @@ class Product(models.Model):
     name = models.CharField(max_length=50, null = True)
     price = models.FloatField(null = True)
     category = models.CharField(max_length =200, null =True , choices = CATEGORY)
-    description = models.CharField(max_length = 200, null = True)
+    description = models.CharField(max_length = 200, null=True, blank =True)
     data_created = models.DateTimeField(auto_now_add=True, null=  True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+    
+
+
 
 
 class Order(models.Model):
-    #customer = 
-    #product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null = True, on_delete=models.SET_NULL)
     STATUS =(
         ('Pending','Pending'),
         ('Out of delivery', 'Out of delivery'),
--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -6,6 +6,6 @@ from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name  ='home'),
-    path('customer/', views.customer, name='customer'),
+    path('customer/<str:pk_test>/', views.customer, name='customer'),
     path('products/', views.products, name  ='products'),
 ]
